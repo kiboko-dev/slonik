4da41ce9a2f8d2fbd6deb9a2a1b20559bb3a5a0f
Konstantin Kaznacheev <kazna.k.rad@yandex.ru>
2024-09-10 04:52:47 +0300

    Правка

--- app/Http/Requests/GetSettingsRequest.php
+++ app/Http/Requests/GetSettingsRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Knuckles\Scribe\Attributes\BodyParam;
 
-#[BodyParam('connectionUuid', 'string', 'UUID соединения', required: true, example: 'Макс')]
+#[BodyParam('connectionUuid', 'string', 'UUID соединения', required: true, example: 'e3b84e41-5c09-39e0-bd31-d17747001df6')]
 class GetSettingsRequest extends FormRequest
 {
     public function authorize(): bool
